d1b1400915d2ab99b1ebb0917198805d251b5df6
Ridmika <ridmika.20210763@iit.ac.lk>
2023-02-12 20:55:24 +0530

    Re-arrange home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Banner1 from "./Banner1"
 import Banner2 from "./Banner2"
-import Banner3 from "./Banner3"
 
 function Home() {
     return (
         <div className="homecontainer">
             <Banner1 />
             <Banner2 />
-            <Banner3 />
         </div>
     )
 }
