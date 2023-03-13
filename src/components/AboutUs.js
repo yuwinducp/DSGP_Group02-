abd729db4be0c86356be38a0fc9a84f5b6474492
Ridmika <ridmika.20210763@iit.ac.lk>
2023-03-13 16:09:32 +0530

    updating about us and what we do pages

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,7 +8,7 @@ function AboutUs() {
         <div style={{backgroundImage:`url(${process.env.PUBLIC_URL + 'assets/B7.jpg'})`}} className="AboutUsContainer">
             <section>
             <div className="about_title"><h2>About Us</h2></div>
-            <p className="about_tale">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+            {/* <p className="about_tale">Lorem ipsum dolor sit amet consectetur adipisicing elite.</p> */}
             </section>
 
             <section class="container_mvo">
@@ -17,21 +17,21 @@ function AboutUs() {
                     <FaBullseye size={64} />
                     </div>
                     <h2>Mission</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    <p>The mission is to develop a user-friendly and reliable web-based platform for Agromatic, a crop prediction and fertilizer recommendation application, with the aim of enabling farmers worldwide to make informed decisions about their crops and optimize their yields while minimizing their environmental impact.</p>
                 </div>
                 <div class="card">
                     <div class="icon">
                         <FaUsers size={64} />
                     </div>
                     <h2>Vision</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    <p>Our vision is to revolutionize farming by empowering farmers worldwide with a user-friendly web platform for crop prediction and fertilizer recommendation. We aim to promote sustainable practices, reduce environmental impact, and improve farmers' livelihoods by providing them with accurate and reliable information to optimize their crop yields.</p>
                 </div>
                 <div class="card">
                     <div class="icon">
                         <FaUserFriends size={64} />
                     </div>
                     <h2>Objectives</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    <p>The objectives of the web-based platform for Agromatic are to provide farmers worldwide with an accessible and accurate solution for crop prediction and fertilizer recommendation, promote sustainable agriculture practices, and empower farmers to make informed decisions that improve their crop yields and livelihoods while minimizing environmental impact.</p>
                 </div>
             </section>
             
@@ -39,31 +39,31 @@ function AboutUs() {
             <section class="container_member">
                 <div class="card">
                     <div class="image">
-                    <img src="/assets/LOGO.png" alt="" />
+                    <img src="/assets/yuwindu.jpeg" alt="" />
                     </div>
                     <h2>Yuwindu Pathirage</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p> */}
                 </div>
                 <div class="card">
                     <div class="image">
-                    <img src="/assets/LOGO.png" alt="" />
+                    <img src="/assets/ridmika.png" alt="" />
                     </div>
                     <h2>Ridmika Hasaranga</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p> */}
                 </div>
                 <div class="card">
                     <div class="image">
                     <img src="/assets/LOGO.png" alt="" />
                     </div>
                     <h2>Amandi Wijesuriya</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p> */}
                 </div>
                 <div class="card">
                     <div class="image">
-                    <img src="/assets/LOGO.png" alt="" />
+                    <img src="/assets/ghajanan.jpeg" alt="" />
                     </div>
                     <h2>Amirthalinham Ghajanan</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p>
+                    {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elite.</p> */}
                 </div>
             </section>
         </div>
--- src/components/WhatWeDo.js
+++ src/components/WhatWeDo.js
@@ -8,20 +8,20 @@ function WhatWeDo() {
             <section className='container_crop'>
                 <div className="whatwedo_title"><h2>Crop Prediction</h2>
                 <img src='assets/cropicon.png' alt="Crop Icon" className="wwd_card__icon"/></div>
-                <p className="whatwedo_tale">Our crop prediction system uses advanced algorithms and historical data to accurately predict the yield and quality of crops.</p>
+                <p className="whatwedo_tale">At Agromatic, our crop prediction system utilizes advanced algorithms and machine learning techniques to accurately forecast crop yields and quality based on historical data and real-time environmental factors. Our system takes into account various factors such as soil quality, weather patterns, and farming practices to provide farmers with precise predictions for their crops. By leveraging machine learning technology, we aim to enable farmers worldwide to make informed decisions that optimize their yields and improve their farming practices.</p>
             </section>
 
             <section className='container_fertilizer'>
                 <div className="whatwedo_title_fp">
                 <img src='assets/fertilizericon.png' alt="Fertilizer Icon" className="wwd_card__icon"/>
                 <h2>Fertilizer Prediction</h2></div>
-                <p className="whatwedo_tale">Our fertilizer prediction system helps farmers optimize the use of fertilizers to improve crop yield and quality while reducing costs.</p>
+                <p className="whatwedo_tale">At Agromatic, our fertilizer prediction system leverages data-driven analytics to provide farmers with personalized fertilizer recommendations that optimize crop yields while minimizing environmental impact and reducing costs. Our system takes into account factors such as soil quality, crop type, and weather patterns to determine the optimal fertilizer type, application rate, and timing. By offering precise and tailored recommendations, we aim to empower farmers to make informed decisions that enhance their crop yields and reduce their environmental footprint.</p>
             </section>
 
             <section className='container_advice'>
                 <div className="whatwedo_title"><h2>Agri Advice</h2>
                 <img src='assets/adviceicon.png' alt="Advice Icon" className="wwd_card__icon"/></div>
-                <p className="whatwedo_tale">Our team of agronomists provides personalized advice and recommendations to farmers to help them achieve their goals and maximize their returns.</p>
+                <p className="whatwedo_tale">At Agromatic, our team of experienced agronomists provides advice and guidance to farmers through a series of informative and educational videos. Our advice videos cover a wide range of topics, including crop management, pest control, irrigation, and soil health, among others. By leveraging the expertise of our agronomists, we aim to empower farmers with the knowledge and tools they need to optimize their farming practices, increase their yields, and maximize their returns.</p>
             </section>
             <section><h1 style={{opacity: '0'}}>What We Do Page</h1></section>
         </div>
